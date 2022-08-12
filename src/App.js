00229a553527cf4e847607b1e847b639666322b4
Cody Denniston <dennistonc4817@gmail.com>
2022-08-11 20:29:12 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -30,20 +30,19 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-  //     setPosts(posts.map(post => {
-  //    if(post.id == postId){
-  //     ...post
-  //     post.likes += 1
-  //    } else {
-  //     return post
-  //    }
-  //      }))
+      setPosts(posts.map(post => {
+    if(post.id === postId){
+      return {...post, likes: post.likes + 1}
+    } 
+      return post
+    
+       }))
    };
 
   return (
     <div className='App'>
       <SearchBar/>
-       <Posts/>
+       <Posts posts={posts} likePost={likePost}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
